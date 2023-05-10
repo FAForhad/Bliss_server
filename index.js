ab38ac3e566c253c4a47702ab0d7f3d589dd5e33
FAForhad <faforhad450@gmail.com>
2023-05-10 21:21:22 +0600

    delete_cure

--- index.js
+++ index.js
@@ -9,17 +9,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.c3txqlb.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mxnk8qu.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
 async function run() {
   try {
     const productCollection = client.db("bliss").collection("products");
+    const usersCollection = client.db("bliss").collection("users");
 
+    // product collection
     app.get("/products", async (req, res) => {
       const query = {};
       const cursor = productCollection.find(query).sort({ time: -1 });
@@ -56,9 +61,53 @@ async function run() {
       }
       const result = await productCollection.find(quary).toArray();
       res.send(result);
-
-      console.log(result);
     });
+    app.delete("/allproduct/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
+    // USER COLLECTION
+
+    app.post("/users", async (req, res) => {
+      const query = req.body;
+      const result = await usersCollection.insertOne(query);
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const cursor = usersCollection.find(query).sort({ time: -1 });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.put("/verifyuser/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body.status;
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          isVerifyed: status,
+        },
+      };
+      const result = await usersCollection.updateOne(query, updatedDoc, options);
+      res.send(result);
+    });
+
+    app.delete("/allusers/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+    //
+    //
+    //
   } finally {
   }
 }
